7afcf4fc87e2b8b8ec4a05fdd5d5379198a3d1fb
Chidi-Bernard Emmanuel <89081878+bernard-rr@users.noreply.github.com>
2023-11-16 18:38:41 +0000

    made the prompt more robust

--- generate_readme.py
+++ generate_readme.py
@@ -1,18 +1,22 @@
-# from langchain.chains import LLMChain
 import os
 import openai
 from dotenv import load_dotenv
 
-
 def generate_readme(api_key, repo_content):
     openai.api_key = api_key
 
-    formatted_prompt = f"Generate a comprehensive README with the correct markdown format for the following GitHub repository content:\n\n{repo_content}"
+    formatted_prompt = (
+        "Create a professional README.md for a GitHub repository. "
+        "The README should be in the correct markdown format, including a Table of Contents, Introduction, Installation, Usage, Features, Contributing, License, and Contact sections. "
+        "The content should reflect the following repository data:\n\n"
+        f"{repo_content}\n\n"
+        "Make sure the README is clear, concise, and well-formatted."
+    )
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo-16k",
         messages=[
-            {"role": "system", "content": "You are a professional assistant skilled at generating README files from GitHub repository content provided."},
+            {"role": "system", "content": "You are a professional assistant skilled at generating README files from GitHub repository content."},
             {"role": "user", "content": formatted_prompt}
         ],
         max_tokens=10000
