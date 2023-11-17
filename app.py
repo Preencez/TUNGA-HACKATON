8cd0bb6bb08bd81bd46e59025e32bbfda06a4a94
asus <princesstoy07@yahoo.com>
2023-11-17 07:28:28 +0100

    final updates

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
 import streamlit as st
 import json
+
+# Placeholder functions (replace these with your actual implementations)
+def find_next_readme_name(user, repo, token):
+    # Your implementation here
+    return "placeholder_readme.md"
+
+def create_commit_readme(user, repo, token, readme_file_name, readme_content):
+    # Your implementation here
+    pass
+
 from utils import parse_github_url, get_repo_files, get_file_content
 from generate_readme import generate_readme
 
@@ -23,9 +33,6 @@ def main():
     st.sidebar.markdown("4. Click the 'Generate README' button.")
     st.sidebar.markdown("5. Review the generated README in the main panel.")
 
-     # Display image above the generated README content
-    #t.image("https://th.bing.com/th/id/OIP.APmJvXP1CyR94wFUG8VgKwHaGh?rs=1&pid=ImgDetMain")
-
     generate_button = st.button("Generate README")
 
     if generate_button and repo_url and token and openai_api_key:
@@ -48,16 +55,12 @@ def main():
             st.text_area("Generated README:", readme_content, height=300)
             st.markdown("Copy the above content and paste it into your README file.")
 
-<<<<<<< HEAD
             user, repo = parse_github_url(repo_url)
             readme_file_name = find_next_readme_name(user, repo, token)
             create_commit_readme(user, repo, token, readme_file_name, readme_content)
 
             st.success(f"README file '{readme_file_name}' successfully created in the repository.")
-            st.success(f"Generated README saved to 'generated_readme.md'.")
-=======
-            st.success("README content generated successfully. Please copy and paste it into your repository.")
->>>>>>> d61be2e5be9300921ba0c94e4ce36df19e4394f8
+        
 
         except Exception as e:
             st.error(f"Error: {e}")
