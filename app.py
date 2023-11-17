c01a472318d34e488ba6f77070e00a400cf48128
Chidi-Bernard Emmanuel <89081878+bernard-rr@users.noreply.github.com>
2023-11-17 18:50:24 +0000

    update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import json
-from utils import parse_github_url, get_repo_files, get_file_content
+from utils import parse_github_url, get_repo_files, get_file_content, create_or_update_readme
 from generate_readme import generate_readme
 
 def main():
